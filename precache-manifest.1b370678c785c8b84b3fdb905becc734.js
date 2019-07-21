e55d807d4321edbf50dfdf3748075e13c7b19ed1
Jingda Mai <jingdamai@gmail.com>
2019-07-21 22:01:30 +0900

    Updates

---
+++ precache-manifest.1b370678c785c8b84b3fdb905becc734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0ebadb87adb7e28f45a1df8182bac7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/react-gh-pages/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d8d54f7d575127b335e0",
+    "url": "/react-gh-pages/static/css/main.4e5351e2.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/react-gh-pages/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "d8d54f7d575127b335e0",
+    "url": "/react-gh-pages/static/js/main.75f19226.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-gh-pages/static/media/SEGA.12a3db8b.woff"
+  }
+]);
